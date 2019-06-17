49c5e4f923b4ecf7fa2a81ae40225a6a52650f61
Zhaoxian Hu <huzhx@uci.edu>
2019-06-17 13:41:48 -0700

    init commit

---
+++ src/com/huzhx/LowArray.java
@@ -0,0 +1,25 @@
+package com.huzhx;
+
+// LowArray.java
+// demonstrates array class with low-level interface
+// to run this program: C>java LowArrayApp
+public class LowArray {
+    // ref to array
+    private long[] array;
+
+    // constructor
+    public LowArray(int size) {
+        // create an array
+        array = new long[size];
+    }
+
+    // set value
+    public void setElem(int index, long value) {
+        array[index] = value;
+    }
+
+    // get value
+    public long getElem(int index) {
+        return array[index];
+    }
+}
---
+++ src/com/huzhx/LowArrayApp.java
@@ -0,0 +1,56 @@
+package com.huzhx;
+
+public class LowArrayApp {
+    public static void main(String[] args) {
+        // reference
+        LowArray array;
+        // create LowArray object
+        array = new LowArray(100);
+        // number of items in array
+        int nElems = 0;
+        // loop variable
+        int j;
+
+        // insert 3 items
+        array.setElem(0, 77);
+        array.setElem(1, 99);
+        array.setElem(2, 44);
+        // now 3 items in array
+        nElems = 3;
+        // display items
+        for (j = 0; j < nElems; j++) {
+            System.out.println(array.getElem(j) + " ");
+        }
+        System.out.println("");
+
+        int searchKey = 26;
+        for (j = 0; j < nElems; j++) {
+            if (array.getElem(j) == searchKey) {
+                break;
+            }
+        }
+        if (j == nElems) {
+            System.out.println("Can't find " + searchKey);
+        } else {
+            System.out.println("Found " + searchKey);
+        }
+
+        // delete value 99
+        // look for it
+        for (j = 0; j < nElems; j++) {
+            if (array.getElem(j) == 99) {
+                break;
+            }
+        }
+        // shift higher ones down
+        for (int k = j; k < nElems; k++) {
+            array.setElem(k, array.getElem(k + 1));
+        }
+        nElems--;
+
+        for (j = 0; j < nElems; j++) {
+            System.out.println(array.getElem(j) + " ");
+        }
+        System.out.println("");
+    }
+}
